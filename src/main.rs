2c0aac3d2740696b9c4435e14122438d58f59a28
Barma Vishwanth <vishwanthbsavp@gmail.com>
2023-04-12 22:34:33 +0530

    block validation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+const DIFFICULTY_PREFIX: &str = "00";
+
 pub struct App{
     pub blocks: Vec,
 }
@@ -9,6 +11,14 @@ pub struct Block{
     pub timestamp: i64,
     pub data: String,
     pub nonce: u64,
+}
+
+fn hash_to_binary_representation(hash: &[u8]) -> String {
+    let mut res: String = String::default();
+    for c in hash{
+        res.push_str(&format!("{:b}", c));
+    }
+    res
 }
 
 impl App{
@@ -27,4 +37,41 @@ impl App{
         };
         self.blocks.push(genesis_block);
     ]}
+
+    fn try_add_block(&mut self, block:Block){
+        let latest_block = self.blocks.last().expect("There is at least one block");
+        if self.is_block_valid(&block, latest_block){
+            self.blocks.push(block);
+        } else{
+            error!("Could not add block - Invalid");
+        }
+    }
+
+    fn is_block_valid(&self, block: &Block, previous_block: &Block) -> bool {
+        if block.previous_hash != previous_block.hash{
+            warn!("Block with id: {} has wrong previous hash", block.id);
+            return false;
+        } else if !hash_to_binary_representation(
+            &hex::decode(&block.hash).expect("Can decode from hex"),
+        ).starts_with(DIFFICULTY_PREFIX){
+            warn!("Block with id: {} has invalid difficulty", block.id);
+            return false;
+        }else if block.id != previous_block.id + 1{
+            warn!(
+                "Block with id: {} is not the next block after the lastest: {}",
+                block.id, previous_block.id
+            ),
+            return false;
+        }else if hex::encode(calculate_hash(
+            block.id,
+            block.timestamp,
+            &block.previous_hash,
+            &block.data,
+            block.nonce,
+        )) != block.hash{
+            warn!("Block with id: {} has invalid hash", block.id);
+            return false;
+        }
+        ture
+    }
 }
